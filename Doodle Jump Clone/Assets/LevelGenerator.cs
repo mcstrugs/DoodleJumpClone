396007004c583f9e48c97b96468e1fa3517ac8b4
mcstrugs <martinsonjoe2@gmail.com>
2017-11-10 08:49:07 -0800

    add game

---
+++ Doodle Jump Clone/Assets/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject platformPrefab;
+
+    public int numberOfPlatforms;
+    public float levelWidth = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+    
+    // Use this for initialization
+	void Start ()
+    {
+		Vector3 spawnPosition = new Vector3();
+		spawnPosition.y -= 3f;
+		float currentThing = 0f;
+		for (int i = 0; i < numberOfPlatforms; i++)
+		{
+			spawnPosition.y += Random.Range(minY, maxY);
+			//spawnPosition.y += 0.2f;
+			//spawnPosition.x = levelWidth*Mathf.Sin(currentThing) + Mathf.Cos(2*currentThing);
+			spawnPosition.x = Random.Range(-levelWidth,levelWidth);
+			Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+			currentThing += 0.2f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
